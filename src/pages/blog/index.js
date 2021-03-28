c18244782a11c2f17132055c59c5c8f9c06150a7
prithipal khalsa <prithipalpsk@gmail.com>
2021-03-28 09:56:04 -0700

    blog conflict

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,34 +1,34 @@
-// import React from 'react'
-//
-// import Layout from '../../components/Layout'
-// import { Link } from 'gatsby'
-// import BlogRoll from '../../components/BlogRoll'
-// import LeftSideBar from '../../components/LeftSideBar'
-//
-// export default class BlogIndexPage extends React.Component {
-//
-//   render() {
-//     return (
-//       <Layout>
-//         <section className="section">
-//           <div className="container">
-//           <div className="columns is-12 is-multiline is-centered">
-//
-//             <div className="column is-one-quarter is-hidden-touch">
-//               <LeftSideBar/>
-//             </div>
-//
-//             <div className="column is-two-thirds  ">
-//                 <BlogRoll />
-//             </div>
-//
-//
-//
-//         </div>
-//           </div>
-//
-//         </section>
-//       </Layout>
-//     )
-//   }
-// }
+import React from 'react'
+
+import Layout from '../../components/Layout'
+import { Link } from 'gatsby'
+import BlogRoll from '../../components/BlogRoll'
+import LeftSideBar from '../../components/LeftSideBar'
+
+export default class BlogIndexPage extends React.Component {
+
+  render() {
+    return (
+      <Layout>
+        <section className="section">
+          <div className="container">
+          <div className="columns is-12 is-multiline is-centered">
+
+            <div className="column is-one-quarter is-hidden-touch">
+              <LeftSideBar/>
+            </div>
+
+            <div className="column is-two-thirds  ">
+                <BlogRoll />
+            </div>
+
+
+
+        </div>
+          </div>
+
+        </section>
+      </Layout>
+    )
+  }
+}
