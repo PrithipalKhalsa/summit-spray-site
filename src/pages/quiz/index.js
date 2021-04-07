cf196ad19e4d16c8c9119c453b677981f6acc425
prithipal khalsa <prithipalpsk@gmail.com>
2021-04-06 20:49:23 -0600

    4

---
+++ src/pages/quiz/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { kebabCase } from 'lodash'
+import { Helmet } from 'react-helmet'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/Layout'
+
+const QuizPage = ({
+  data: {
+    allMarkdownRemark: { group },
+    site: {
+      siteMetadata: { title },
+    },
+  },
+}) => (
+  <Layout>
+  <p>Quiz!</p>
+  </Layout>
+)
+
+export default QuizPage
+
+export const quizPageQuery = graphql`
+  query QuizPage {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(limit: 1000) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`
