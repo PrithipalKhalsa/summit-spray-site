39aa88a94a2db8345f071e177c1f15d48d6262e8
daniel-bogart <daniel.bogart17@gmail.com>
2022-02-12 00:57:38 -0800

    rebuilding nav component / styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,97 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'gatsby'
 import logo from '../img/logo-wide.png'
 import logoBig from '../img/big-logo.png'
+import classNames from 'classnames';
 
-const Navbar = class extends React.Component {
+const Navbar = (props) => {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      active: false,
-      navBarActiveClass: '',
-    }
-  }
+  const [active, setActive] = useState(false);
+  const [navBarActiveClass, setClass] = useState('');
 
-  toggleHamburger = () => {
-    // toggle the active boolean in the state
-    this.setState(
-      {
-        active: !this.state.active,
-      },
-      // after state has been updated,
-      () => {
-        // set the class in state for the navbar accordingly
-        this.state.active
-          ? this.setState({
-              navBarActiveClass: 'is-active',
-            })
-          : this.setState({
-              navBarActiveClass: '',
-            })
-      }
-    )
-  }
 
-  render() {
-
-    return (
-
-      <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-      <div className="big-logo">  <Link to="/" title="Logo"><img src={logoBig} alt="summit-spray-big"/></Link></div>
+  // toggleHamburger = () => {
+  // }
+  return (
+    <nav
+      className="ssp-navbar"
+      role="navigation"
+      aria-label="main-navigation"
+    >
+      <div>
+        <div className="big-logo">  <Link to="/" title="Logo">
+          <img src={logoBig}    alt="summit-spray-big"/></Link>
+        </div>
         <div className="container">
           <div className="navbar-brand">
-
-            {/* Hamburger menu */}
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
-            >
-              <span />
-              <span />
-              <span />
-            </div>
+              {/* Hamburger menu */}
+              <div
+                className={`navbar-burger burger ${navBarActiveClass}`}
+                data-target="navMenu"
+                onClick={() => toggleHamburger()}
+              >
+                <span />
+                <span />
+                <span />
+              </div>
           </div>
           <div
             id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
+            className={`navbar-menu ${navBarActiveClass}`}
           >
             <div className="navbar-start">
-              <Link className="navbar-item" activeClassName="active-nav" to="/stories">
+              <Link className="navbar-item" activeClassName="active-nav"to="/    stories">
                 Latest Stories
               </Link>
-              <Link className="navbar-item" activeClassName="active-nav" to="/news">
+              <Link className="navbar-item" activeClassName="active-nav" to="/ news">
                 News
               </Link>
-              <Link className="navbar-item" activeClassName="active-nav" to="/climbing">
+              <Link className="navbar-item" activeClassName="active-nav"to="/    climbing">
                 Climbing
               </Link>
-              <Link className="navbar-item"  activeClassName="active-nav" to="/lifestyle">
+              <Link className="navbar-item"  activeClassName="active-nav"to="/   lifestyle">
                 Lifestyle
               </Link>
               <Link className="navbar-item"  activeClassName="active-nav" to="/ski">
                 Ski
               </Link>
-
-              <Link className="navbar-item" activeClassName="active-nav" to="/about">
+              <Link className="navbar-item" activeClassName="active-nav"to="/    about">
                 About
               </Link>
-              <Link className="navbar-item" activeClassName="active-nav" to="/contact">
+              <Link className="navbar-item" activeClassName="active-nav"to="/    contact">
                 Contact
               </Link>
             </div>
-
           </div>
         </div>
-      </nav>
-
-    )
-  }
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
